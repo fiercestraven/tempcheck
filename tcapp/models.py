36a61dc45a653731eb3d953dc5356af8c3c99979
Frances Veit <veitf@yahoo.com>
2022-10-08 10:28:19 +0100

    created models

--- tcapp/models.py
+++ tcapp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Lecturer(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    # fv - figure out how to add security here
+    lecturer_password = models.CharField(max_length=200)
+
+class Module(models.Model):
+    module_name = models.CharField(max_length=200)
+    module_description = models.TextField(default = "")
+    # insert foreign key to lecturer_id
+    lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
+
+class Lecture(models.Model):
+    lecture_name = models.CharField(max_length=200)
+    lecture_description = models.TextField(default = "")
+    lecture_date = models.DateTimeField('date of lecture')
+    # insert foreign key to module_id
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    username = models.CharField(max_length=200)
+    # fv - figure out how to add security here
+    student_password = models.CharField(max_length=200)
+
+class Student_Module(models.Model):
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+class Student_Lecture(models.Model):
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+
+class Ping(models.Model):
+    ping_date = models.DateTimeField('date and time of ping')
+    # create foreign keys to both student_id and lecture_id
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
