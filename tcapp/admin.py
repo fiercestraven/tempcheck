7ecb383cf50a654a9bac374b9067ec77a03054f5
Frances Veit <veitf@yahoo.com>
2022-10-08 10:53:27 +0100

    changes to models, admin

--- tcapp/admin.py
+++ tcapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Instructor, Module, Lecture, Student, Ping
 
 # Register your models here.
+admin.site.register(Instructor)
+admin.site.register(Module)
+admin.site.register(Lecture)
+admin.site.register(Student)
+admin.site.register(Ping)
--- tcapp/models.py
+++ tcapp/models.py
@@ -2,18 +2,22 @@ from django.db import models
 
 # Create your models here.
 
-class Lecturer(models.Model):
+class Instructor(models.Model):
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200)
     username = models.CharField(max_length=200)
     # fv - figure out how to add security here
-    lecturer_password = models.CharField(max_length=200)
+    instructor_password = models.CharField(max_length=200)
+    def __str__(self):
+        return self.username
 
 class Module(models.Model):
     module_name = models.CharField(max_length=200)
     module_description = models.TextField(default = "")
-    # insert foreign key to lecturer_id
-    lecturer = models.ForeignKey(Lecturer, on_delete=models.CASCADE)
+    # insert foreign key to instructor_id
+    instructor = models.ForeignKey(Instructor, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.module_name
 
 class Lecture(models.Model):
     lecture_name = models.CharField(max_length=200)
@@ -21,6 +25,8 @@ class Lecture(models.Model):
     lecture_date = models.DateTimeField('date of lecture')
     # insert foreign key to module_id
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.lecture_name
 
 class Student(models.Model):
     first_name = models.CharField(max_length=200)
@@ -28,6 +34,8 @@ class Student(models.Model):
     username = models.CharField(max_length=200)
     # fv - figure out how to add security here
     student_password = models.CharField(max_length=200)
+    def __str__(self):
+        return self.username
 
 class Student_Module(models.Model):
     module = models.ForeignKey(Module, on_delete=models.CASCADE)
@@ -42,3 +50,5 @@ class Ping(models.Model):
     # create foreign keys to both student_id and lecture_id
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     lecture = models.ForeignKey(Lecture, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.ping_date
