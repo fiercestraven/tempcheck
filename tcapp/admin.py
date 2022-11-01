50c210c595c3048e62a6ec4b14f2a9e9b4aae6c1
Frances Veit <veitf@yahoo.com>
2022-11-01 17:58:27 +0000

    fixed Question/Results and added block titles

--- tcapp/admin.py
+++ tcapp/admin.py
@@ -10,20 +10,33 @@ class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,               {'fields': ['question_text']}),
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-        # (None,               {'fields': ['lecture.module_id']}),
-        (None,               {'fields': ['lecture_id']}),
+        (None,               {'fields': ['lecture']}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'lecture_id', 'pub_date')
+    list_display = ('question_text', 'lecture', 'pub_date')
 
 class UserAdmin(admin.ModelAdmin):
     fields = ['first_name', 'last_name', 'username', 'email', 'password', 'is_staff']
     list_filter = ('is_staff',)
+    list_display = ('username', 'first_name', 'last_name', 'email', 'is_staff')
+
+class ModuleAdmin(admin.ModelAdmin):
+    fields = ['module_name', 'instructor', 'is_active']
+    list_filter = ('is_active',)
+    list_display = ('module_name', 'instructor', 'is_active')
+
+class LectureAdmin(admin.ModelAdmin):
+    fields = ['module', 'lecture_name', 'lecture_date']
+    list_display = ('module', 'lecture_name', 'lecture_date')
+
+class PingAdmin(admin.ModelAdmin):
+    fields = ['student', 'lecture', 'ping_date']
+    list_display = ('student', 'lecture', 'ping_date')
 
 # Register your models here.
-admin.site.register(Module)
-admin.site.register(Lecture)
-admin.site.register(Ping)
+admin.site.register(Module, ModuleAdmin)
+admin.site.register(Lecture, LectureAdmin)
+admin.site.register(Ping, PingAdmin)
 admin.site.register(Question, QuestionAdmin)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
