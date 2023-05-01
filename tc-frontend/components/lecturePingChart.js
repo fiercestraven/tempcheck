f8ffcf02ae17abf217c5fda35809eae0579fde59
Frances Veit <veitf@yahoo.com>
2023-05-01 20:12:02 +0100

    pulled out lecture ping chart component

---
+++ tc-frontend/components/lecturePingChart.js
@@ -0,0 +1,135 @@
+import * as Plot from '@observablehq/plot';
+import * as d3 from "d3";
+import { addTooltips } from '../lib/plotTooltips';
+import { CurrentUserContext } from '../context/auth';
+import { useContext, useEffect, useState, useRef } from 'react';
+// https://www.npmjs.com/package/react-csv-downloader
+import CsvDownloader from 'react-csv-downloader';
+
+export default function LecturePingChart({ lectureName }) {
+    const { userData, userDataLoaded } = useContext(CurrentUserContext);
+    const [pingData, setPingData] = useState([]);
+    const [pingFetchComplete, setPingFetchComplete] = useState(false);
+    const dotChartRef = useRef();
+
+    // fetch data
+    useEffect(() => {
+        setPingFetchComplete(false);
+
+        async function fetchData() {
+            // target correct API endpoint and bring in pings for chosen lecture
+            const res = await fetch(`http://localhost:8000/tcapp/api/lectures/${lectureName}/pings/`, {
+                headers: {
+                    'Authorization': `Bearer ${userData.access_token}`,
+                },
+            });
+
+            const data = await res.json();
+            // turn ping date into an actual date for graphing purposes
+            const parsedData = data.map(datum => {
+                datum.ping_date = new Date(datum.ping_date);
+                datum.normalized = new Date(
+                    // ignoring year, month, day - any given lecture occurs on only one day
+                    // convert hours and minutes into milliseconds
+                    (datum.ping_date.getUTCHours() * 60 * 60 * 1000) +
+                    (datum.ping_date.getUTCMinutes() * 60 * 1000)
+                    // ignoring seconds - too granular for the chart
+                )
+                // make a version of the date for csv export that is more easily readable
+                datum.readableDate = datum.ping_date.toISOString().slice(11, 19);
+                return datum;
+            });
+            setPingData(parsedData);
+            setPingFetchComplete(true);
+        }
+
+        if (userDataLoaded && lectureName) {
+            fetchData();
+        }
+    }, [userDataLoaded, lectureName]);
+
+    // set up ping visualisation chart
+    useEffect(() => {
+        // https://observablehq.com/@observablehq/plot?collection=@observablehq/plot
+        const chart = addTooltips(Plot.plot({
+            style: { background: 'transparent' },
+            width: 1000,
+            height: 100,
+            marks: [
+                Plot.dot(pingData, Plot.binX(
+                    { r: 'count', title: (pings) => `${pings.length} Ping${pings.length == 1 ? '' : 's'}` },
+                    { x: 'normalized', thresholds: d3.timeMinute.every(1) }
+                )),
+                Plot.frame({ stroke: 'white' }),
+                // make marks every 5 min on x-axis
+                Plot.axisX({
+                    label: 'UTC Time',
+                    interval: d3.timeMinute.every(5)
+                }),
+            ],
+            // provide visual padding for first and last pings
+            insetLeft: 30,
+            insetRight: 30,
+        }), {
+            stroke: 'white',
+            fill: 'gray',
+            'stroke-width': 4,
+        });
+        dotChartRef?.current?.append(chart);
+        return () => chart?.remove();
+    }, [dotChartRef.current, pingData]);
+
+
+    // set up column names for csv export file
+    const csvColumns = [
+        { id: 'readableDate', displayName: 'Ping Date' },
+        { id: 'student', displayName: 'Student' },
+        { id: 'lecture', displayName: 'Lecture' }
+    ];
+
+    if (!lectureName) {
+        return;
+    }
+
+    if (!pingFetchComplete) {
+        return (<div>Loading...</div>)
+    }
+
+    if (pingData.length == 0) {
+        return (
+            <div>
+                <p></p>
+                <p className="user-message">There are no pings associated with this lecture.</p>
+            </div>
+        )
+    }
+
+    // we know fetch is done and we have data to display
+    return (
+        <>
+            <p></p>
+            <div ref={dotChartRef}></div>
+            <table>
+                <tr>
+                    <th>Ping Date</th>
+                    <th>Student</th>
+                    <th>Lecture</th>
+                </tr>
+                {pingData.map((ping) => (
+                    <tr>
+                        {/* return string of date here instead of object, and slice it to return only the time, not date */}
+                        <td>{ping.readableDate}</td>
+                        <td>{ping.student}</td>
+                        <td>{ping.lecture}</td>
+                    </tr>
+                ))}
+                {!pingData.length &&
+                    <tr>
+                        <td>There are no pings to display.</td>
+                    </tr>
+                }
+            </table>
+            <CsvDownloader className="w-30 mt-4 mb-5 btn btn-md btn-light" datas={pingData} columns={csvColumns} filename={`${pingData[0].lecture}.csv`} />
+        </>
+    )
+}
--- tc-frontend/pages/stats.js
+++ tc-frontend/pages/stats.js
@@ -1,27 +1,25 @@
 
 import * as Plot from '@observablehq/plot';
-import * as d3 from "d3";
 import { addTooltips } from '../lib/plotTooltips';
 import Head from 'next/head';
 import Layout from '../components/layout';
 import Header from '../components/header';
+import LecturePingChart from '../components/lecturePingChart';
 import { CurrentUserContext } from '../context/auth';
 import { useContext, useEffect, useState, useRef } from 'react';
 import { useRouter } from 'next/router';
-// https://www.npmjs.com/package/react-csv-downloader
-import CsvDownloader from 'react-csv-downloader';
 
 export default function Stats() {
   const { userData, logoutUser, userDataLoaded } = useContext(CurrentUserContext);
   const [moduleData, setModuleData] = useState([]);
   const [lectureData, setLectureData] = useState();
   const [pingData, setPingData] = useState([]);
-  const [pingFetchComplete, setPingFetchComplete] = useState(false);
   const [profileData, setProfileData] = useState();
   const [lectureFetchComplete, setLectureFetchComplete] = useState(false);
   const router = useRouter();
   const dotChartRef = useRef();
   const pingsPerModuleChartRef = useRef();
+  const [selectedLecture, setSelectedLecture] = useState();
 
   useEffect(() => {
     async function getModuleData() {
@@ -50,40 +48,6 @@ export default function Stats() {
       getProfileData();
     }
   }, [userData]);
-
-  // set up ping visualisation chart
-  // https://observablehq.com/@observablehq/plot?collection=@observablehq/plot
-  useEffect(() => {
-    console.debug("pingData is:", pingData);
-    console.debug("dotChartRef is:", dotChartRef);
-    const chart = addTooltips(Plot.plot({
-      style: { background: 'transparent' },
-      width: 1000,
-      height: 100,
-      marks: [
-        // fv - remove this comment: Plot.ruleX(pingData, {x: 'normalized', strokeOpacity: 0.2, thresholds: d3.timeMinute.every(1)}),Plot.frame({stroke: 'white'}),
-        Plot.dot(pingData, Plot.binX(
-          { r: 'count', title: (pings) => `${pings.length} Ping${pings.length == 1 ? '' : 's'}` },
-          { x: 'normalized', thresholds: d3.timeMinute.every(1) }
-        )),
-        Plot.frame({ stroke: 'white' }),
-        // make marks every 5 min on x-axis
-        Plot.axisX({
-          label: 'UTC Time',
-          interval: d3.timeMinute.every(5)
-        }),
-      ],
-      // provide visual padding for first and last pings
-      insetLeft: 30,
-      insetRight: 30,
-    }), {
-      stroke: 'white',
-      fill: 'gray',
-      'stroke-width': 4,
-    });
-    dotChartRef?.current?.append(chart);
-    return () => chart?.remove();
-  }, [dotChartRef.current, pingData]);
 
   // set up avg pings per module bar chart
   useEffect(() => {
@@ -149,6 +113,7 @@ export default function Stats() {
 
   // take user-selected data for module and look up lecture info
   async function handleModuleChange(event) {
+    setSelectedLecture(undefined);
     console.log(event.target.value);
 
     // target correct API endpoint and bring in lectures for the chosen module
@@ -162,43 +127,6 @@ export default function Stats() {
     setLectureFetchComplete(true);
     console.log("Lecture data is ", data);
   }
-
-  async function handleLectureChange(event) {
-    console.log(event.target.value);
-
-    // target correct API endpoint and bring in pings for chosen lecture
-    const res = await fetch(`http://localhost:8000/tcapp/api/lectures/${event.target.value}/pings/`, {
-      headers: {
-        'Authorization': `Bearer ${userData.access_token}`,
-      },
-    });
-    const data = await res.json();
-    // turn ping date into an actual date for graphing purposes
-    const parsedData = data.map(datum => {
-      datum.ping_date = new Date(datum.ping_date);
-      datum.normalized = new Date(
-        // ignoring year, month, day - any given lecture occurs on only one day
-        // convert hours and minutes into milliseconds
-        (datum.ping_date.getUTCHours() * 60 * 60 * 1000) +
-        (datum.ping_date.getUTCMinutes() * 60 * 1000)
-        // ignoring seconds - too granular for the chart
-      )
-      // make a version of the date for csv export that is more easily readable
-      datum.readableDate = datum.ping_date.toISOString().slice(11, 19);
-      return datum;
-    });
-    setPingData(parsedData);
-    setPingFetchComplete(true);
-    console.log(pingData);
-  }
-
-  // set up column names for csv export file
-  const csvColumns = [
-    { id: 'readableDate', displayName: 'Ping Date' },
-    { id: 'student', displayName: 'Student' },
-    { id: 'lecture', displayName: 'Lecture' }
-  ]
-
 
   return (
     <Layout>
@@ -221,7 +149,7 @@ export default function Stats() {
               onChange={handleModuleChange}
             >
               {/* on using defaultValue: https://stackoverflow.com/questions/44786669/warning-use-the-defaultvalue-or-value-props-on-select-instead-of-setting */}
-              <option defaultValue>Select a Module</option>
+              <option defaultValue value="">Select a Module</option>
               {moduleData.map((module) => (
                 <option value={module.module_shortname} key={module.module_shortname}>
                   {module.module_shortname}
@@ -245,9 +173,9 @@ export default function Stats() {
                 <select
                   className="form-select"
                   aria-label="Lecture selection"
-                  onChange={handleLectureChange}
+                  onChange={(event) => { setSelectedLecture(event.target.value) }}
                 >
-                  <option defaultValue>Select a Lecture</option>
+                  <option defaultValue value="">Select a Lecture</option>
                   {lectureData.lectures.map((lecture) => (
                     <option value={lecture.lecture_shortname} key={lecture.lecture_shortname}>
                       {lecture.lecture_name}
@@ -259,48 +187,7 @@ export default function Stats() {
                 </select>
 
                 {/* chart here */}
-                {/* make sure pingData.length is a Boolean to prevent 0 being printed to page */}
-                {pingFetchComplete && !!pingData.length &&
-                  <div>
-                    <p></p>
-                    <div ref={dotChartRef}></div>
-                  </div>
-                }
-
-                {/* if no pings for chosen lecture, display message */}
-                {pingFetchComplete && !pingData.length &&
-                  <div>
-                    <p></p>
-                    <p className="user-message">There are no pings associated with this lecture.</p>
-                  </div>
-                }
-              </div>
-            }
-
-            {/* table for ping csv here */}
-            {pingFetchComplete && pingData.length &&
-              <div>
-                <table>
-                  <tr>
-                    <th>Ping Date</th>
-                    <th>Student</th>
-                    <th>Lecture</th>
-                  </tr>
-                  {pingData.map((ping) => (
-                    <tr>
-                      {/* return string of date here instead of object, and slice it to return only the time, not date */}
-                      <td>{ping.readableDate}</td>
-                      <td>{ping.student}</td>
-                      <td>{ping.lecture}</td>
-                    </tr>
-                  ))}
-                  {!pingData.length &&
-                    <tr>
-                      <td>There are no pings to display.</td>
-                    </tr>
-                  }
-                </table>
-                <CsvDownloader className="w-30 mt-4 mb-5 btn btn-md btn-light" datas={pingData} columns={csvColumns} filename={`${pingData[0].lecture}.csv`} />
+                <LecturePingChart lectureName={selectedLecture} />
               </div>
             }
           </div>
