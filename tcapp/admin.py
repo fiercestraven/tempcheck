44b6a381355b1c138bfe4b0c694d26b914c96cbb
Frances Veit <veitf@yahoo.com>
2023-05-08 21:18:04 +0100

    csv uploader cleanup

--- tcapp/admin.py
+++ tcapp/admin.py
@@ -93,33 +93,27 @@ class UserAdmin(StaffPermission, UserAdmin):
             if not csv_file.name.endswith(".csv"):
                 messages.warning(request, "The wrong file type was uploaded.")
                 return HttpResponseRedirect(request.path_info)
-            # rows = TextIOWrapper(csv_file, encoding="utf-8", newline="")
+
             # fix end of line \r and end of file errors
             file_data = csv_file.read().decode("utf-8").replace("\r", "").strip()
+
             # split into list of lines
             csv_data = file_data.split("\n")
-            # iterate over lines and split into fields
 
+            # iterate over lines and split into fields
             row_count = 0
-            # form_errors = []
             for x in csv_data:
                 row_count += 1
-                # fv - note: here, put into a form and then run validation - if working, can delete from fields=x.split(",")
-                # fv - could try following this: https://djangosource.com/django-csv-upload.html
-                # form = csvImportForm(x)
-                # if not form.is_valid():
-                #     form_errors = form.errors
-                #     break
-                # form.save()
-                # below is current code (non-form), working:
                 fields = x.split(",")
                 # note - username in the Django model is unique; will use that and let it auto-fill pk
-                created = User.objects.update_or_create(
+                User.objects.update_or_create(
                     username=fields[0],
-                    first_name=fields[1],
-                    last_name=fields[2],
-                    email=fields[3],
-                    is_staff=fields[4],
+                    defaults=dict(
+                        first_name=fields[1],
+                        last_name=fields[2],
+                        email=fields[3],
+                        is_staff=fields[4],
+                    ),
                 )
             # reverse function: https://stackoverflow.com/questions/56711082/reverse-django-admin-urls
             url = reverse("admin:auth_user_changelist")
